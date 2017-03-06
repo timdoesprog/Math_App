8d87881885fdaa149a55414d6ddab63d06bae372
Tim Betz <writingtumblr@gmx.de>
2017-03-06 15:13:19 +0100

    First Working Version
    The core methods are implemented. A quiz can be started and the user
    gets asked 10 random questions and at the end you can see how many you
    got right and how long it took to answer all questions.

--- quiz.py
+++ quiz.py
@@ -5,32 +5,51 @@
 
 from questions import Add, Multiply
 import datetime
+import random
 
 
 class Quiz:
     questions = []
     answers = []
+    points = 0
 
     def __init__(self):
         # generate 10 random questions with numbers from 1 to 10
-        # add these questions into self.questions
+        ran_gen = random.randint
+        for i in range(10):
+            question = random.choice([Add(ran_gen(0, 10), ran_gen(0, 10)),
+                                      Multiply(ran_gen(0, 10), ran_gen(0, 10))
+                                      ])
+            # add these questions into self.questions
+            self.questions.append(question)
 
     def take_quiz(self):
         # log the start time
+        start = datetime.datetime.now()
         # ask all of the questions
-        # log if they got the question right
+        for question in self.questions:
+            self.ask(question)
         # log the end time
+        end = datetime.datetime.now()
         # show summary (time and correct answers/points)
+        self.summary(start, end)
 
     def ask(self, question):
         # log the start time
         # capture the answer
+        answer = int(input("{}\n".format(question.text)))
         # check the answer
+        if answer == question.answer:
+            self.points += 1
         # log end time
         # if the answer is right -> True
         # else -> False
         # send back the elapsed times
 
-    def summary(self):
+    def summary(self, start, end):
         # print how many answers right/amount of questions
+        print("You got {} of {} questions right!".format
+              (self.points, len(self.questions)))
         # print the total time for the quiz
+        delta = end - start
+        print("Your total time was {} seconds!".format(delta.seconds))
